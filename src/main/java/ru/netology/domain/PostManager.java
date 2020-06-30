1f6af9f76031a1d8d815b1ed32ca9e60d2fa303f
Gulyuki Aleksey <gulyuki_a@bk.ru>
2020-06-30 19:43:10 +0300

    v2

---
+++ src/main/java/ru/netology/domain/PostManager.java
@@ -0,0 +1,15 @@
+package ru.netology.domain;
+
+public class PostManager {
+    private Post[] post = new Post[0];
+
+    public Post[] search(int ownerId, String query, String[] items) {
+
+        return null;
+    }
+
+    public Post[] delete (int ownerId, int postId,  boolean owners_only) {
+
+        return null;
+    }
+}
